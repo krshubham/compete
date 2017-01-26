347e121bc65bf3b71ec006457b9c4fb1cb84486f
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-26 20:55:43 +0530

    tricoin init

--- codechef/Practice/beginner/tricoin.cpp
+++ codechef/Practice/beginner/tricoin.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 typedef long long ll;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	ios_base::sync_with_stdio(false);
 	ll t;
 	cin>>t;
